b2360b930f4b807a35098076232a9d69fbab77ba
Casey K <tehcnicolour@gmail.com>
2022-08-25 22:12:26 -0700

    no smooth

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -31,8 +31,8 @@ const Layout = ({ children }: LayoutProps) => {
       <S.Wrapper>
         <Header />
         <S.Content>
-          <SmoothScroll>{children}</SmoothScroll>
-          {/* {children} */}
+          {/* <SmoothScroll>{children}</SmoothScroll> */}
+          {children}
         </S.Content>
       </S.Wrapper>
       <Footer />
